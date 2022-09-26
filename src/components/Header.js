7244b10abe4f201dea46ad52e591b17493f2290e
Guyddo <guyddogl@gmail.com>
2022-09-26 15:21:40 -0300

    Requisitos 9 e 10

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header(props) {
+  const [showInputSearch, setShowInputSearch] = useState(false);
+
+  const [inputs, setInputs] = useState({
+    search: '',
+  });
+
   const { title, showSearch, showProfile } = props;
+
+  const handleInputSearch = ({ target }) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [target.name]: target.value,
+    }));
+  };
+
   return (
     <div>
       <h2 data-testid="page-title">{title}</h2>
@@ -21,10 +36,26 @@ export default function Header(props) {
       <br />
       <br />
       {showSearch && (
-        <img
-          alt="search"
-          src={ searchIcon }
-          data-testid="search-top-btn"
+        <>
+          <img
+            alt="search"
+            src={ searchIcon }
+            onClick={ () => setShowInputSearch(!showInputSearch) }
+            role="presentation"
+            style={ { cursor: 'pointer' } }
+            data-testid="search-top-btn"
+          />
+          <SearchBar />
+        </>
+      )}
+      {showInputSearch && (
+        <input
+          type="text"
+          name="search"
+          value={ inputs.search }
+          placeholder="email"
+          onChange={ handleInputSearch }
+          data-testid="search-input"
         />
       )}
     </div>
