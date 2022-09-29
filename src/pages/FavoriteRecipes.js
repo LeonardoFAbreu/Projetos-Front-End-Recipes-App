40957c95ab8a4bfff19f3f7028f2da71ee9a500b
Guyddo <guyddogl@gmail.com>
2022-09-29 12:10:02 -0300

    Requisitos 55 e 56

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
 import { shareRecipe, saveFavorite } from '../helpers/services';
 
-export default function FavoriteRecipes() {
+export default function FavoriteRecipes({ history }) {
   const [shared, setShared] = useState(false);
 
   const [isFavorite, setIsFavorite] = useState(false);
+
+  const [favoritesRecipes, setFavoritesRecipes] = useState([]);
+
+  useEffect(() => {
+    setFavoritesRecipes(JSON.parse(localStorage.getItem('favoriteRecipes')) || []);
+  }, [isFavorite]);
 
   const checkType = (recipe) => {
     if (recipe.type === 'drink') return `${recipe.alcoholicOrNot}`;
@@ -31,24 +39,68 @@ export default function FavoriteRecipes() {
     return setShared(true);
   };
 
-  const favorites = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+  const handleClickFilter = (type) => {
+    const favorites = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    if (type === 'meal') {
+      return setFavoritesRecipes(favorites.filter((e) => e.type === 'meal'));
+    }
+    if (type === 'drink') {
+      return setFavoritesRecipes(favorites.filter((e) => e.type === 'drink'));
+    }
+    setFavoritesRecipes(favorites);
+  };
+
+  const favorites = favoritesRecipes;
 
   return (
     <div>
       <Header title="Favorite Recipes" showSearch={ false } showProfile />
+      <button
+        type="button"
+        onClick={ () => handleClickFilter('all') }
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        onClick={ () => handleClickFilter('meal') }
+        data-testid="filter-by-meal-btn"
+      >
+        Meals
+      </button>
+      <button
+        type="button"
+        onClick={ () => handleClickFilter('drink') }
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+      </button>
       {favorites.map((recipe, index) => (
         <div key={ recipe.id }>
-          <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+          <Link
+            to={ `/${recipe.type}s/${recipe.id}` }
+            data-testid={ `${index}-horizontal-name` }
+          >
+            {recipe.name}
+          </Link>
           <p
             data-testid={ `${index}-horizontal-top-text` }
           >
             {checkType(recipe)}
           </p>
-          <img
-            src={ recipe.image }
-            alt={ recipe.name }
-            data-testid={ `${index}-horizontal-image` }
-          />
+          <Link
+            to={ `/${recipe.type}s/${recipe.id}` }
+          >
+            <img
+              src={ recipe.image }
+              alt={ recipe.name }
+              onClick={ () => history.push(`/${recipe.type}s/${recipe.id}`) }
+              role="presentation"
+              style={ { maxHeight: '200px' } }
+              data-testid={ `${index}-horizontal-image` }
+            />
+          </Link>
           <img
             src={ blackHeartIcon }
             alt="Favorite"
@@ -69,3 +121,9 @@ export default function FavoriteRecipes() {
     </div>
   );
 }
+
+FavoriteRecipes.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
